871a7290d3eddb1b28edea9cddeeda652ff918b2
ABHIJIT649 <abhijitmohanty440@gmail.com>
2025-07-09 22:32:10 +0530

    add js part

--- script.js
+++ script.js
@@ -3,3 +3,9 @@
 
 
 
+
+setInterval(function () {
+    let date = new Date ()
+    // console.log(date.toLocaleDateString());
+    clock.innerHTML = date.toLocaleTimeString();
+} , 1000) //1000 mean 1 sec
